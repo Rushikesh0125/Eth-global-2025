1c9157159eca51c2ab54c8021ecf35d5b42f3beb
Rushikesh0125 <jadhav.rushikesh.ggl@gmail.com>
2025-09-28 03:51:48 +0530

    chore: deployed apis to serverless

--- backend/index.js
+++ backend/index.js
@@ -269,6 +269,67 @@ app.post('/reputation/batch', authenticateApiKey, async (req, res) => {
     }
 });
 
+
+const selfBackendVerifier = new SelfBackendVerifier(
+    "test-scope", // scope string
+    "https://ke2j1k45n1.execute-api.eu-west-3.amazonaws.com/dev/api/verify", // endpoint (your backend verification API)
+    false, // mockPassport → false = testnet, realPassport → true = mainnet
+    AllIds, // allowed attestation IDs map
+    new DefaultConfigStore({
+        // config store (see separate docs)
+        minimumAge: 18,
+        excludedCountries: [],
+        ofac: true,
+    }),
+    "uuid"
+);
+
+app.post("/api/verify", async (req, res) => {
+    try {
+        const { attestationId, proof, publicSignals, userContextData } = req.body;
+        if (!proof || !publicSignals || !attestationId || !userContextData) {
+            return res.status(200).json({
+                status: "error",
+                result: false,
+                reason:
+                    "Proof, publicSignals, attestationId and userContextData are required",
+            });
+        }
+
+        const result = await selfBackendVerifier.verify(
+            attestationId,
+            proof,
+            publicSignals,
+            userContextData
+        );
+
+        const nullifier = result.discloseOutput.nullifier;
+
+        const { isValid, isMinimumAgeValid, isOfacValid } = result.isValidDetails;
+        if (!isValid || !isMinimumAgeValid || isOfacValid) {
+            let reason = "Verification failed";
+            if (!isMinimumAgeValid) reason = "Minimum age verification failed";
+            if (isOfacValid) reason = "OFAC verification failed";
+            return res.status(200).json({
+                status: "error",
+                result: false,
+                reason,
+            });
+        }
+
+        return res.status(200).json({
+            status: "success",
+            result: true,
+        });
+    } catch (error) {
+        return res.status(200).json({
+            status: "error",
+            result: false,
+            reason: error instanceof Error ? error.message : "Unknown error",
+        });
+    }
+});
+
 // Error handling middleware
 app.use((req, res, next) => {
     res.status(404).json({
